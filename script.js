e097f11ad673bbb6e04930dd5114531a220bc003
nicomellon <nmellon94@gmail.com>
2021-12-23 01:13:00 +0100

    adds some styles

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
 /** HTML elements **/
 const startBtn = document.querySelector("#start-btn");
-const bpmInput = document.querySelector("#bpm-input");
-const stepsForm = document.querySelector("#steps-form");
+const bpmInput = document.querySelector(".bpm-input");
+const stepsSection = document.querySelector(".steps-section");
 const addStepBtn = document.querySelector("#add-step-btn");
 const removeStepBtn = document.querySelector("#remove-step-btn");
 const stepsCount = document.querySelector("#steps-count");
 
 function removeStep() {
-  if (stepsForm.childElementCount > 1)
-    stepsForm.removeChild(stepsForm.lastChild);
+  if (stepsSection.childElementCount > 1)
+    stepsSection.removeChild(stepsSection.lastChild);
   updateStepSpan();
 }
 
-function addStep(checked) {
+function createStep(checked) {
   const stepInput = document.createElement("input");
   stepInput.type = "checkbox";
   stepInput.checked = checked;
-  stepsForm.appendChild(stepInput);
+  return stepInput;
+}
+
+function addStep(checked) {
+  stepsSection.appendChild(createStep(checked));
   updateStepSpan();
 }
 
 function updateStepSpan() {
-  stepsCount.innerText = stepsForm.childElementCount;
+  stepsCount.innerText = stepsSection.childElementCount;
 }
 
 /**  metronome control functions **/
 function startMetronome() {
   /* get metronome params */
-  const bpm = bpmInput.value;
+  const bpm = bpmInput.value || 60;
   // const steps = stepsForm.childElementCount;
-  const steps = stepsForm.childNodes;
+  const steps = stepsSection.childNodes;
   console.log(steps);
   /* create metronome & start */
   window.metronome = new Metronome(bpm, steps);
